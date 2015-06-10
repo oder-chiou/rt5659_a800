3d5ef49fbcca1019c9990bed352b43ff85b3bc6e
oder_chiou <oder_chiou@realtek.com>
2015-06-10 16:02:35 +0800

    Minor fix
    Signed-off-by: oder_chiou <oder_chiou@realtek.com>

--- rt5659.c
+++ rt5659.c
@@ -3078,6 +3078,7 @@ static int rt5659_monol_filter_event(struct snd_soc_dapm_widget *w,
 	struct snd_kcontrol *kcontrol, int event)
 {
 	struct snd_soc_codec *codec = w->codec;
+	struct rt5659_priv *rt5659 = snd_soc_codec_get_drvdata(codec);
 	static unsigned int sto_dac_mixer, mono_dac_mixer;
 
 	pr_debug("%s\n", __func__);
